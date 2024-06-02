34dde33f285beb038a7411b9c02edb6877bc290e
Jaomar92 <65635396+Jaomar92@users.noreply.github.com>
2024-06-03 00:35:12 +0700

    tested out cloudinary

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,27 @@
-import Image from "next/image";
+"use client";
+
+import Navbar from "@/components/navbar";
+import { CldImage, CldUploadButton } from "next-cloudinary";
 
 export default function Home() {
-  return <h1>This is Jeff working on next and cloudinary</h1>;
+  return (
+    <>
+      <Navbar />
+      <main className="container mx-auto h-[200vh]">
+        <p>This is Jeff working on next and cloudinary</p>
+        <CldUploadButton uploadPreset="hthvgkln" />
+        <div className="relative h-[300px] w-[500px]">
+          <CldImage
+            fill
+            src="zkd6psqwlhss13wcfvzg"
+            alt="Description of my image"
+            className="-z-10"
+          />
+          <p className="z-10 text-white w-full h-full flex items-center justify-center">
+            This is an Image
+          </p>
+        </div>
+      </main>
+    </>
+  );
 }
---
+++ src/components/navbar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="bg-emerald-700 sticky top-0 z-50">
+      <div className="container h-12 text-white text-center text-3xl font-bold">
+        Navbar
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
